c15a12259796476baa7c7ef7daf94d0173c2e8ef
Hydra <lordxzaodeo@gmail.com>
2026-07-07 02:11:42 +0700

    better build system

--- build.sh
+++ build.sh
@@ -2,87 +2,198 @@
 
 . ./config.sh
 
+ANDROID_LIB="$ANDROID_SDK_PATH/platforms/android-$ANDROID_API/android.jar"
+TOOLS="$ANDROID_SDK_PATH/build-tools/$BUILD_TOOLS_VER/lib/d8.jar"
+R8=(java -cp "$TOOLS" com.android.tools.r8.R8)
+D8=(java -cp "$TOOLS" com.android.tools.r8.D8)
+AIDL="$ANDROID_SDK_PATH/build-tools/$BUILD_TOOLS_VER/aidl"
+CMAKE="$ANDROID_SDK_PATH/cmake/$CMAKE_VERSION/bin/cmake"
+NINJA="$ANDROID_SDK_PATH/cmake/$CMAKE_VERSION/bin/ninja"
+CMAKE_TOOLCHAIN="$ANDROID_NDK_PATH/build/cmake/android.toolchain.cmake"
+RESULT_DIR="$BUILD/termux-daemon-$ANDROID_ABI-$BUILD_TYPE"
+SCRIPT_NAME=$(basename "$OUTPUT_APK" .apk)
+
 set -e
 [[ $DEBUG == true ]] && set -x
 
-get_android_version() {
-    case $1 in
-        29) echo "10" ;;
-        30) echo "11" ;;
-        31) echo "12" ;;
-        32|33) echo "13" ;;
-        *) echo $1 ;;
-    esac
+resolve_deps ()
+{
+  if [[ -f $ANDROID_LIB ]]; then
+    echo "[*] found android.jar with api '$ANDROID_API'"
+  else
+    echo "[!] cannot found android.jar with api '$ANDROID_API' are you sure you have installed it?"
+    exit 127
+  fi
+
+  if [[ -f $AIDL ]]; then
+    echo "[*] found aidl"
+  else
+    echo "[!] cannot found aidl in $AIDL"
+    exit 127
+  fi
+
+  if [[ -f $TOOLS ]]; then
+    echo "[*] found d8 and r8"
+  else
+    echo "[!] cannot found d8 and r8 in $TOOLS"
+    exit 127
+  fi
+
 }
 
-if [[ ! -f "$ANDROID" ]]; then
-    mkdir -p "$ANDROID_DIR"
-    ZIP="${TMPDIR:-/tmp}/platform-${ANDROID_API}.zip"
-    DL_URL=$(python fetch_android.py "$ANDROID_API")
-    echo "[*] Downloading $ANDROID"
-    # is there any way to just android.jar not whole android platforms
-    curl -L -C - -o "$ZIP" "$DL_URL"
-    ANDROID_VERSION=$(get_android_version $ANDROID_API)
-    unzip -j "$ZIP" "android-${ANDROID_VERSION}/android.jar" -d "$ANDROID_DIR"
-    rm "$ZIP"
-fi
+clean_and_make ()
+{
+  echo "[*] cleaning build output"
+  # NOTE: since CMakeLists or ndk-build know if source is change
+  # we dont need to remove cpp build dir
+  rm -rf "$JAVA_BUILD_DIR" "$OUTPUT_APK"
+  mkdir -p "$JAVA_BUILD_DIR"
+}
 
-echo "[*] $ANDROID ready"
+process_aidl ()
+{
+  echo "[*] processing aidl"
 
-rm -rf "$BUILD"
-rm -f classes.dex "$OUTPUT_APK"
-mkdir -p "$BUILD"
-
-echo "[*] Processing AIDL"
-find src/java -name '*.aidl' | while read -r file; do
+  find "$JAVA_SRC_DIR" -name "*.aidl" | while read -r file; do
     out="${file%.aidl}.java"
 
     # Skip parcelable declarations
-    grep -q '^parcelable ' "$file" && continue
+    grep -q "^parcelable " "$file" && continue
+    "$AIDL" -I "$JAVA_SRC_DIR" "$file" "$out"
+  done
 
-    "$AIDL" \
-        -I src/java \
-        "$file" \
-        "$out"
-done
+}
 
-echo "[*] Compiling to class"
-find src/java -name '*.java' > "$BUILD/sources.txt"
-javac \
-    -cp "$ANDROID" \
-    -d "$BUILD" \
-    --release 17 \
-    "@$BUILD/sources.txt"
+process_java ()
+{
+  echo "[*] Compiling to class"
+  SOURCE_FILE="$JAVA_BUILD_DIR/sources.txt"
+  find "$JAVA_SRC_DIR" -name "*.java" > "$SOURCE_FILE"
+  javac -cp "$ANDROID_LIB" -d "$JAVA_BUILD_DIR" --release 17 "@$SOURCE_FILE"
+}
 
-rm $BUILD/sources.txt
-
-if [[ $MINIFIED == true ]]; then
+process_dex ()
+{
+  if [[ $BUILD_TYPE == release ]]; then
     echo "[*] Compiling to dex (minify)"
 
     PG_CONF=""
     if [[ -f "$RULES" ]]; then
-        PG_CONF="--pg-conf $RULES"
+      PG_CONF="--pg-conf $RULES"
     else
-        echo "[!] Minify dex need $RULES file"
-        exit 1
+      echo "[!] build minify need $RULES file"
+      exit 127
     fi
 
-    find "$BUILD/$PACKAGE_NAME" -name '*.class' -print0 |
-        xargs -0 "$R8" \
-            --release \
-            --lib "$ANDROID" \
-            --output . \
-            $PG_CONF
-else
+    find "$JAVA_BUILD_DIR" -name "*.class" -print0 |
+      xargs -0 "${R8[@]}" --release --lib "$ANDROID_LIB" --output \
+      "$JAVA_BUILD_DIR" $PG_CONF
+  elif [[ $BUILD_TYPE == debug ]]; then
     echo "[*] Compiling to dex (no minify)"
+    find "$JAVA_BUILD_DIR" -name "*.class" -print0 |
+      xargs -0 "${D8[@]}" --lib "$ANDROID_LIB" --output \
+      "$JAVA_BUILD_DIR"
+  else
+    echo "[*] Unknow build type: $BUILD_TYPE"
+    exit 1
+  fi
+}
 
-    find "$BUILD/$PACKAGE_NAME" -name '*.class' -print0 |
-        xargs -0 "$D8" \
-            --lib "$ANDROID"
-fi
+__create_script_runner ()
+{
+  ENTRY_CLASS=$(echo "$PACKAGE_NAME" | tr / .).Main
+  content=$(cat << EOF
+#!/system/bin/env sh
 
-echo "[*] Packaging dex into apk"
-zip -j "$OUTPUT_APK" classes.dex
-rm -f classes.dex
+HERE="\$(dirname "\$(readlink -f "\$0")")"
 
-echo "[*] Build complete -+> $OUTPUT_APK"
+export PATH=/system/bin/
+export CLASSPATH=\$HERE/../share/$SCRIPT_NAME/$OUTPUT_APK
+exec app_process64 -Xmx10m -Xnoimage-dex2oat / "$ENTRY_CLASS" "\$@"
+EOF
+  )
+
+  echo "$content" > "$1"
+  chmod +x "$1"
+}
+
+package_all ()
+{
+  echo "[*] Packaging..."
+  SHARE_DIR="$RESULT_DIR/share/$SCRIPT_NAME"
+  BIN_DIR="$RESULT_DIR/bin"
+  mkdir -p "$SHARE_DIR"
+  mkdir -p "$BIN_DIR"
+  zip -jq "$SHARE_DIR/$OUTPUT_APK" "$JAVA_BUILD_DIR/classes.dex"
+  "$CMAKE" --install "$CPP_BUILD_DIR"
+  __create_script_runner "$BIN_DIR/$SCRIPT_NAME"
+  cp LICENSE "$SHARE_DIR"
+
+  echo "[*] Done build termux-daemon-$ANDROID_ABI-$BUILD_TYPE"
+}
+
+__resolve_native_tools ()
+{
+  if [[ $1 == "cmake" ]]; then
+    if [[ -f $CMAKE ]]; then
+      echo "[*] found cmake with version $CMAKE_VERSION"
+    else
+      echo "[!] cannot found cmake with version $CMAKE_VERSION"
+      exit 127
+    fi
+  elif [[ $1 == "ndk-build" ]]; then
+    if [[ -f $ANDROID_NDK_PATH/ndk-build ]]; then
+      echo "[*] found ndk-build"
+    else
+      echo "[!] cannot found ndk-build at $ANDROID_NDK_PATH"
+    fi
+  else
+    echo "[*] usage __resolve_native_tools [ndk-build|cmake]"
+    exit 1
+  fi
+
+}
+
+process_cpp ()
+{
+  if [[ -d $CPP_SRC_DIR ]]; then
+    echo "[*] found cpp dir"
+    if [[ -f $CPP_SRC_DIR/CMakeLists.txt ]]; then
+      __resolve_native_tools cmake
+      echo "[*] Using cmake build system"
+       "$CMAKE" "$CPP_SRC_DIR" -B "$CPP_BUILD_DIR" \
+         -DCMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN" \
+         -DANDROID_ABI="$ANDROID_ABI" \
+         -DANDROID_PLATFORM="$ANDROID_API" \
+         -DCMAKE_BUILD_TYPE="${BUILD_TYPE^}" \
+         -DCMAKE_INSTALL_PREFIX="$RESULT_DIR/" \
+         -GNinja
+
+       # NOTE: "string^" means uppercase first char
+
+       "$NINJA" -C "$CPP_BUILD_DIR"
+    elif [[ -f $CPP_SRC_DIR/Android.mk ]]; then
+      echo "[*] Using ndk build system"
+      __resolve_native_tools ndk-build
+      echo "TODO: handle ndk build system"
+    else
+      echo "[!] cannot detect the build system are used"
+      exit 1
+    fi
+  fi
+
+}
+
+main ()
+{
+  clean_and_make
+  process_cpp
+  resolve_deps
+  process_aidl
+  process_java
+  process_dex
+  package_all
+
+}
+
+main
--- config.sh
+++ config.sh
@@ -1,17 +1,20 @@
 # Change this to your own configuration
 
 DEBUG=false
-MINIFIED=false
+BUILD_TYPE=release
+BUILD_TOOLS_VER=35.0.0
+CMAKE_VERSION=4.3.4
 ANDROID_API=29
-ANDROID_DIR=lib/android-$ANDROID_API
-ANDROID=$ANDROID_DIR/android.jar
-
-# i don't know why but if i use d8 or r8 from termux repo i keep getting a error
-# so i use d8 from my android-sdk
-D8=$HOME/opt/android-sdk/build-tools/35.0.0/d8
-AIDL=$HOME/opt/android-sdk/build-tools/35.0.0/aidl
-R8=$HOME/opt/r8/8.8.46/r8
+# NOTE: for now lets just focused only one arch
+ANDROID_ABI=arm64-v8a
+ANDROID_SDK_PATH=$ANDROID_SDK_ROOT
+ANDROID_NDK_PATH=$ANDROID_NDK_HOME
 RULES=src/proguard-rules.pro
-BUILD=build/
+JAVA_SRC_DIR=src/java
+CPP_SRC_DIR=src/cpp
+BUILD=build
+JAVA_BUILD_DIR=$BUILD/java
+CPP_BUILD_DIR=$BUILD/cpp/$ANDROID_ABI
 PACKAGE_NAME=org/termux/daemon
 OUTPUT_APK=termux-daemon.apk
+OUTPUT_NATIVE=android-daemon
